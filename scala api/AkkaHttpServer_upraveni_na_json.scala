31eb2c9281ea7c51e8058165eafb041381ead3a4
Kompaso3659 <77006634+Kompaso3659@users.noreply.github.com>
2023-05-09 16:20:31 +0200

    Update AkkaHttpServer_upraveni_na_json.scala

--- scala api/AkkaHttpServer_upraveni_na_json.scala
+++ scala api/AkkaHttpServer_upraveni_na_json.scala
@@ -15,7 +15,7 @@ case class NasaApiResponse(date: String, explanation: String, media_type: String
 class NasaApiClient(apiKey: String = "")(implicit system: ActorSystem, mat: Materializer) {
   private val apiBaseUrl = "https://api.nasa.gov/planetary/apod"
 
- def getImageOfTheDay(date: String): Future[NasaApiResponse] = {
+  def getImageOfTheDay(date: String): Future[NasaApiResponse] = {
     val requestUrl = s"$apiBaseUrl?date=$date&api_key=$apiKey"
     val request = HttpRequest(HttpMethods.GET, requestUrl)
     val responseFuture = Http().singleRequest(request)
@@ -30,6 +30,7 @@ class NasaApiClient(apiKey: String = "")(implicit system: ActorSystem, mat: Mate
         Future.failed(new RuntimeException(s"Unexpected status code ${response.status}"))
     })
   }
+}
 
 object AkkaHttpServer extends App {
   implicit val system = ActorSystem()
@@ -41,49 +42,14 @@ object AkkaHttpServer extends App {
 
   val route = {
     concat(
-      path("hello_to") {
-        get {
-          parameters("name".as[String]) { name =>
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"Hello to $name"))
-          }
-        }
-      },
-      path("hello") {
-        get {
-          onComplete(Future {
-            "ourString"
-          }) {
-            case Success(value) => complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Hello to everyone"))
-            case Failure(exception) => complete(InternalServerError, s"An error occurred: ${exception.getMessage}")
-          }
-        }
-      },
       path("nasa") {
-  get {
-    parameters("date".as[String]) { date =>
-      onComplete(nasaApiClient.getImageOfTheDay(date)) {
-        case Success(response) =>
-          complete(response.toJson.prettyPrint)
-        case Failure(ex) =>
-          complete(HttpResponse(StatusCodes.InternalServerError, entity = s"Failed to retrieve NASA data: ${ex.getMessage}"))
-                    s"""
-                       |<h2>${response.title}</h2>
-                       |<p>${response.explanation}</p>
-                       |<img src="${response.url}"/>
-                       |""".stripMargin))
-                } else if (response.media_type == "video") {
-                  complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
-                    s"""
-                       |<h2>${response.title}</h2>
-                       |<p>${response.explanation}</p>
-                       |<iframe width="560" height="315" src="${response.url}" frameborder="0" allowfullscreen></iframe>
-                       |""".stripMargin))
-                } else {
-                  complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Unknown media type"))
-                }
+        get {
+          parameters("date".as[String]) { date =>
+            onComplete(nasaApiClient.getImageOfTheDay(date)) {
+              case Success(response) =>
+                complete(response.toJson.prettyPrint)
               case Failure(ex) =>
-                complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,
-                  s"Failed to retrieve NASA data: ${ex.getMessage}"))
+                complete(HttpResponse(StatusCodes.InternalServerError, entity = s"Failed to retrieve NASA data: ${ex.getMessage}"))
             }
           }
         }
@@ -92,25 +58,7 @@ object AkkaHttpServer extends App {
         get {
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Response from server"))
         }
-      },
-      pathPrefix("user" / LongNumber)(userId =>
-        concat(
-          pathEndOrSingleSlash {
-            get {
-              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"user detail of user $userId"))
-            }
-          },
-          path("delete") {
-            decodeRequest {
-              post {
-                entity(as[String]) { ent: String =>
-                  complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, s"deleting user $userId with params $ent"))
-                }
-              }
-            }
-          }
-        )
-      )
+      }
     )
   }
 
@@ -119,6 +67,14 @@ object AkkaHttpServer extends App {
     _ = println(s"Server running on ${binding.localAddress.getHostName}:${binding.localAddress.getPort}")
   } yield binding
 
-  StdIn.readLine()
-  bindingFut.flatMap(_.unbind()).andThen(_ => system.terminate())
+  bindingFut.onComplete {
+    case Success(binding) =>
+      println(s"Server is listening on ${binding.localAddress}")
+    case Failure(ex) =>
+      println(s"Server could not start!")
+      ex.printStackTrace()
+      system.terminate()
+  }
+
+  Await.result(system.whenTerminated, Duration.Inf)
 }
